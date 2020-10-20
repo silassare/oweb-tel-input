cfd4de92d7ce8c31e3860e48ddb69459c31d1ed1
Emile Silas SARE <emile.silas@gmail.com>
2020-10-20 22:21:08 +0100

    removed

--- types/countiesList.d.ts
+++
@@ -1,3 +0,0 @@
-export declare type tCountryListItem = [string, string, string, number | null, string[] | null];
-declare const _default: [string, string, string, number | null, string[] | null][];
-export default _default;
